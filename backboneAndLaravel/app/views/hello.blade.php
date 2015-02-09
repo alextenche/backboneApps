70aaf897f9c6dbfb8b099144862e71ef901ce59f
alexTenche <alex.tenche@gmail.com>
2015-02-09 23:33:17 +0200

    slow progress on backboneAndLaravel

---
+++ backboneAndLaravel/app/views/hello.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Backbone and Laravel</title>
+</head>
+<body>
+
+	<h1>Hello World!</h1>
+
+	<script type="js/lib/jquery.js"></script>
+	<script type="js/lib/underscore.js"></script>
+	<script type="js/lib/backbone.js"></script>
+
+	<script>
+	(function(){
+		window.App = {
+			Models: {},
+			Views: {},
+			Collections:{}
+		};
+		App.Models.Task = Backbone.Model.extend({
+			
+		});
+	});
+	</script>
+
+</body>
+</html>
