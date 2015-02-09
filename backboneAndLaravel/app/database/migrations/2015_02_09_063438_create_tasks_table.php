7c6fdbd7c8b68c2cad916870323d7a5b0ef93614
alexTenche <alex.tenche@gmail.com>
2015-02-09 08:48:28 +0200

    started backboneAndLaravel

---
+++ backboneAndLaravel/app/database/migrations/2015_02_09_063438_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tasks', function($table)
+		{
+			$table->increments('id');
+			$table->string('title');
+			$table->boolean('completed')->default(0);
+
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{	
+		Schema::drop('tasks');
+	}
+	
+
+}
---
+++ backboneAndLaravel/app/database/seeds/tasks.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+	array(
+		'title' => 'go to store'
+	),
+	array(
+		'title' => 'finish backbone tut'
+	),
+	array(
+		'title' => 'get some sleep'
+	)
+);
