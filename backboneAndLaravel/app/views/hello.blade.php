be0d0c8241d0ea200d3a32d78d7f2fdce7650fc1
alexTenche <alex.tenche@gmail.com>
2015-02-11 16:41:56 +0200

    finished backboneAndLaravel

--- backboneAndLaravel/app/views/hello.blade.php
+++ backboneAndLaravel/app/views/hello.blade.php
@@ -19,13 +19,56 @@
 			Collections:{}
 		};
 
+
 		App.Models.Task = Backbone.Model.extend({
 			defaults: {
 				title: '',
 				completed: 0
-			},
-			urlRoot: 'tasks'	
+			}
 		});
+
+
+		App.Collections.Tasks = Backbone.Collection.extend({
+			model: App.Models.Task,
+			url: 'tasks'
+		});
+
+
+		App.Views.Tasks = Backbone.View.extend({
+			tagName : 'ul',
+
+			initialize: function(){
+				this.collection.on('add', this.addOne, this);
+			},
+
+			render: function(){
+				this.$el.empty();
+				this.collection.each(this.addOne, this);
+				return this;
+			},
+
+			addOne: function(task){
+				var task = new App.Views.Task({ model: task });
+				this.$el.append( task.render().el );
+			}
+			
+		});
+
+
+		App.Views.Task = Backbone.View.extend({
+			tagName: 'li',
+
+			initialize: function(){
+				this.model.on('destroy', this.remove, this);
+			},
+
+			render: function(){
+				this.$el.html( this.model.get('title') );
+				return this;
+			}
+		});
+
+
 	})();
 	</script>
 
